5d2a9f6df9993832687c978695e3e989e89cb778
imThiTran <tranvanthitk113@gmail.com>
2021-09-25 12:52:02 +0700

    xong nut chi tiet quan ly don hang

--- public/js/admin-quanly.js
+++ public/js/admin-quanly.js
@@ -1,8 +1,6 @@
 var modalEdit = document.getElementById("modal-edit");
 var modalAdd = document.getElementById("modal-add-item");
-var modalInfo1 = document.getElementById("modal-info-order1");
-var modalInfo2 = document.getElementById("modal-info-order2");
-var modalInfo3 = document.getElementById("modal-info-order3");
+var modalInfo = document.getElementById("modal-info-order");
 
 $('#btn-add-item').on('click', function(){
   modalAdd.style.display = "block";
@@ -19,18 +17,10 @@ $('.close-footer').on('click',function(){
     modalAdd.style.display = "none";
     
 });
-$('.close-footer-info').on('click',function(){
-  modalInfo1.style.display = "none";
-  modalInfo2.style.display = "none";
-  modalInfo3.style.display = "none";
-  
-});
+
 $('#btn-save-change-item').on('click',function(){
     
-    modalEdit1.style.display = "none";
-    modalInfo2.style.display = "none";
-    modalInfo3.style.display = "none";
-    
+    modalEdit.style.display = "none"; 
     alert('Chỉnh sửa hoàn tất!');
 });
 window.onclick = function (event) {
@@ -47,12 +37,18 @@ $('.btn-xoa').on('click',function(){
 $('#btn-print').on('click',function(){
   window.print();
 });
-$('.info-order1').on('click',function(){
-  modalInfo1.style.display = "block";
-});
-$('.info-order2').on('click',function(){
-  modalInfo2.style.display = "block";
-});
-$('.info-order3').on('click',function(){
-  modalInfo3.style.display = "block";
-});
+
+
+$('.info-order').each(function () {
+  var $this = $(this);
+  var modalInfo1 = $this.parent().find('.modal');
+  var CloseModal = $this.parent().find('.close-footer-info');
+  
+  $this.on('click',function(){
+    modalInfo1.css("display","block"); 
+  })
+  CloseModal.on('click',function(){
+    modalInfo1.css("display","none"); 
+  })
+})
+  
