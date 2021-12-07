d3a367b9cc2ca1ad1a8b5e6b908ef87b18469031
Bubu124 <nhoznhatk5@gmail.com>
2021-12-07 19:44:18 +0700

    thieu validate cho 2 cai input

--- routes/admin-statistics.js
+++ routes/admin-statistics.js
@@ -9,14 +9,46 @@ function daysInMonth(month, year){
 
 router.get('/',function(req,res){
     var date = new Date()
-    res.render('admin/admin-statistic',{
-        data:"",
-        month:date.getMonth()+1,
-        year:date.getFullYear()
-    })
+    var month = date.getMonth()+1
+    var year = date.getFullYear()
+    var days = daysInMonth(month,year)
+    var datamonth = new Array(days)
+    var datayear = new Array(12)
+    var newbill1 = []
+    var newbill2 = []
+    Bill.find({}, function(err,bills){
+        bills.forEach(bill => {            
+            if((bill.createAt.getMonth()+1)==month && (bill.createAt.getFullYear())==year) newbill1.push(bill)
+        });
+        for(let i=0;i<datamonth.length;i++){
+            datamonth[i]=0
+            newbill1.forEach(bill => {
+                if((bill.createAt.getDate()-1)==i){
+                    datamonth[i]+=bill.totalPrice
+                }
+            })
+        }        
+        bills.forEach(bill1 => {
+            if( (bill1.createAt.getFullYear())==year) newbill2.push(bill1)
+        });
+        for(let i=0;i<datayear.length;i++){
+            datayear[i]=0
+            newbill2.forEach(bill => {
+                if((bill.createAt.getMonth())==i){
+                    datayear[i]+=bill.totalPrice
+                }
+            })
+        }
+        res.render('admin/admin-statistic',{
+            datamonth:datamonth,
+            datayear:datayear,
+            month:month,
+            year:year
+        })
+    })    
 })
 
-router.post('/',(req,res) =>{
+router.post('/month',(req,res) =>{
     var year = req.body.getmonth.slice(0,4)
     var month = req.body.getmonth.slice(5,7)
     var days = daysInMonth(month,year)
@@ -37,6 +69,33 @@ router.post('/',(req,res) =>{
         res.send({
             data: data,
             month: month,
+            year: year
+        })
+    })    
+})
+
+router.post('/year',(req,res) =>{
+    var year = req.body.getyear
+    var months = []
+    for(let i=0;i<=11;i++){
+        months.push(i)
+    }    
+    var newbill = []
+    var data = new Array(months.length)
+    Bill.find({}, function(err,bills){
+        bills.forEach(bill => {
+            if( (bill.createAt.getFullYear())==year) newbill.push(bill)
+        });
+        for(let i=0;i<data.length;i++){
+            data[i]=0
+            newbill.forEach(bill => {
+                if((bill.createAt.getMonth())==i){
+                    data[i]+=bill.totalPrice
+                }
+            })
+        }        
+        res.send({
+            data: data,            
             year: year
         })
     })    
