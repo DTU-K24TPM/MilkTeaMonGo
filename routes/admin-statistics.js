e383b655efe45db5b1da79199e9f9af709a9acdf
Bubu124 <nhoznhatk5@gmail.com>
2021-12-07 15:10:15 +0700

    thong ke trong 1 thang

--- routes/admin-statistics.js
+++ routes/admin-statistics.js
@@ -1,14 +1,45 @@
 var express = require('express')
 var router = express.Router();
 
+var Bill = require('../models/bill')
+
+function daysInMonth(month, year){
+    return new Date(year,month,0).getDate();
+}
+
 router.get('/',function(req,res){
-    res.render('admin/admin-statistic')
+    var date = new Date()
+    res.render('admin/admin-statistic',{
+        data:"",
+        month:date.getMonth()+1,
+        year:date.getFullYear()
+    })
 })
 
-router.post('/',(req,res) =>{    
+router.post('/',(req,res) =>{
     var year = req.body.getmonth.slice(0,4)
     var month = req.body.getmonth.slice(5,7)
-    res.send(year+"--------"+month)
+    var days = daysInMonth(month,year)
+    var newbill = []
+    var data = new Array(days)
+    Bill.find({}, function(err,bills){
+        bills.forEach(bill => {            
+            if((bill.createAt.getMonth()+1)==month && (bill.createAt.getFullYear())==year) newbill.push(bill)
+        });
+        for(let i=0;i<data.length;i++){
+            data[i]=0
+            newbill.forEach(bill => {
+                if((bill.createAt.getDate()-1)==i){
+                    data[i]+=bill.totalPrice
+                }
+            })
+        }        
+        res.send({
+            data: data,
+            month: month,
+            year: year
+        })
+    })    
 })
 
 module.exports = router;
